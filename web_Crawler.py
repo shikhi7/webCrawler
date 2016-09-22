69796b706417dca69e49594b245b5d75103230df
shikhi7 <mshikhar29@gmail.com>
2016-09-22 23:28:27 +0530

    firstStep

---
+++ web_Crawler.py
@@ -0,0 +1,28 @@
+import requests
+
+links = []
+i=0
+
+def crawl(sourceCode):
+	next_link = getNextTarget(sourceCode)
+	if (next_link==''):
+		i = i+1
+		crawl(getPage(links[i]))
+	else:
+		if next_link in links:
+			crawl(sourceCode[(next_link[1]):])
+		else:
+			links.append(next_link)
+			crawl(sourceCode[(next_link[1]):])
+
+
+def getPage(pageLink):
+	return requests.get(pageLink)
+
+def getNextTarget(sourceCode):
+	startLink = sourceCode.find("<a href=") + 9
+	endLink = sourceCode.find('"', startLink)
+	return sourceCode[startLink:endLink]
+
+
+	
