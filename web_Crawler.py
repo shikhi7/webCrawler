ee40d1bf8db7f01acf23bd57002d2ac5825678d4
shikhi7 <mshikhar29@gmail.com>
2016-10-16 17:32:41 +0530

    third commit

--- web_Crawler.py
+++ web_Crawler.py
@@ -4,18 +4,22 @@ seedPage = raw_input('Enter the link of your seed page link: ')   #determines th
 links = [seedPage]	#list containing all the links
 countOnLinks=1	#keeps count on total count of entries in 'links' list
 indexOfCurrentLink=0	#stores the index of the link (in 'links' list) that is currently being crawled
+searchContent = {}   #dictionary that stores keywords of search as keys and list of links whose sourcecode had its mention as value.
 
 while True:
 	try:
 		thresholdNumber = int(raw_input('Enter your threshold number: '))   #determines the number of links starting from index 0 that will be crawled.
+		break
 	except ValueError:
 		print("Enter a valid positive integer")
+
+#defining functipons-
 
 def crawl(sourceCode):
 	next_link = getNextTarget(sourceCode)
 	if (next_link==''):
 		indexOfCurrentLink += 1
-		crawl(getPage(links[i]))
+		crawl(getSource(links[i]))
 	else:
 		if next_link in links:
 			crawl(sourceCode[(next_link[1]):])
@@ -24,9 +28,7 @@ def crawl(sourceCode):
 			countOnLinks += 1
 			crawl(sourceCode[(next_link[1]):])
 
-
-#function to extract the source code from its link
-def getPage(pageLink):
+def getSource(pageLink): #function to extract the source code from its link
 	return requests.get(pageLink)
 
 def getNextTarget(sourceCode):
@@ -34,8 +36,19 @@ def getNextTarget(sourceCode):
 	endLink = sourceCode.find('"', startLink)   #ending index of the new link
 	return sourceCode[startLink:endLink]
 
+def updateDictionary(pageLink, searchContent):    #this function updates keywords of a particular page source provided by the page link.
+	sourcecode = getSource(pageLink)
+	wordsList = sourceCode.split()
+	for word in wordsList:
+		if word in searchContent.keys():
+			(searchContent['word']).append(pageLink)
+		else:
+			searchContent.update({'word':[]})
+			(searchContent['word']).append(pageLink)
+
 # main program starts from here-
 
-seedPageSource = getPage(seedPage)
-crawl(seedPage)
-
+while (indexOfCurrentLink < thresholdNumber) :
+	crawl(getSource(links[indexOfCurrentLink]))
+	updateDictionary(links[indexOfCurrentLink], searchContent)
+	indexOfCurrentLink += 1
